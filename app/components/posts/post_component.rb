2272d11a4bf5976b290e4006d8c47ae918b718e7
Oscar Tellez <oscartzgz@gmail.com>
2023-03-12 15:56:44 -0600

    feat: Improve UI for posts

--- app/components/posts/post_component.rb
+++ app/components/posts/post_component.rb
@@ -2,6 +2,18 @@
 
 class Posts::PostComponent < ViewComponent::Base
   def initialize(post:)
+    super
     @post = post
+  end
+
+  def post_content_style(content)
+    case content.length
+    when 1..50
+      'text-4xl font-bold py-16'
+    when 51..150
+      'text-2xl font-bold  py-16'
+    else
+      'text-md font-normal py-8'
+    end
   end
 end
