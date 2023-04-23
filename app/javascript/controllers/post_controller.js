1bf93eeed4b7508c0fe051f24438a6df7ff900aa
Oscar Tellez <oscartzgz@gmail.com>
2023-04-22 20:44:45 -0600

    feat: post can have an image

--- app/javascript/controllers/post_controller.js
+++ app/javascript/controllers/post_controller.js
@@ -2,18 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="post"
 export default class extends Controller {
-  static targets = ['textInput', 'submitButton']
+  static targets = ['textInput', 'submitButton', 'imageInput', 'image']
   
   connect() {
   }
 
   validateSubmit() {
-    console.log('validating')
-    console.log(this.textInputTarget)
-    if(this.textInputTarget.value.length > 0) {
-      this.submitButtonTarget.disabled = false
-    } else {
-      this.submitButtonTarget.disabled = true
-    }
+    const valid = !(this.textInputTarget.value.length > 0)
+    this.submitButtonTarget.disabled = valid
+  }
+
+  selectImage() {
+    this.imageInputTarget.click()
+  }
+  
+  // Updates img tag when new image is selected
+  tempUpdateImage() {
+    const file = this.imageInputTarget.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => this.imageTarget.src = reader.result;
   }
 }
