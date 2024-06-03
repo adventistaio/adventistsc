74e73893628d17cb01520994bdf5c886d25bb5a4
Oscar Tellez <oscartzgz@gmail.com>
2024-06-03 10:50:47 -0600

    Create article model

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  def posts_background
-    case Time.current.hour
-    when 5..9 then   'text-white bg-gradient-to-r from-indigo-900 via-indigo-500 to-sky-200'
-    when 10..14 then 'bg-gradient-to-r from-sky-300 via-sky-200 to-yellow-200'
-    when 17..19 then 'bg-gradient-to-r from-yellow-300 via-yellow-200 to-orange-300'
-    when 20..21 then 'bg-gradient-to-r from-orange-400 via-orange-300 to-indigo-400'
-    else
-      'text-white bg-gradient-to-r from-indigo-800 via-indigo-900 to-gray-800'
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_031404) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_03_165032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_031404) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "state"
+    t.string "title"
+    t.text "content"
+    t.datetime "published_at"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -115,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_031404) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "users"
   add_foreign_key "email_verification_tokens", "users"
   add_foreign_key "password_reset_tokens", "users"
